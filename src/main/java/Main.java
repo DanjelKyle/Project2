9062577e5be39b446ee8a21d11039ecdb6b814fa
DanjelKyle <107757601+DanjelKyle@users.noreply.github.com>
2022-06-18 15:54:58 +0200

    kam shtu nje person

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Person p1=Person.builder().emer("kyle").mosha(25).build();
         System.out.println(p1);
+        System.out.println();
     }
 }
